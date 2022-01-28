7ff3150aa6e3af41b01cfddff3c83e01264f036a
ParsaYadollahi <parsa.yadollahi@mail.mcgill.ca>
2022-01-27 20:52:04 -0500

    mmmmm

--- A1/src/ca/mcgill/ecse420/a1/MatrixMultiplication.java
+++ A1/src/ca/mcgill/ecse420/a1/MatrixMultiplication.java
@@ -5,19 +5,19 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class MatrixMultiplication {
-	
+
 	private static final int NUMBER_THREADS = 1;
 	private static final int MATRIX_SIZE = 2000;
 
-        public static void main(String[] args) {
-		
+	public static void main(String[] args) {
+
 		// Generate two random matrices, same size
 		double[][] a = generateRandomMatrix(MATRIX_SIZE, MATRIX_SIZE);
 		double[][] b = generateRandomMatrix(MATRIX_SIZE, MATRIX_SIZE);
 		sequentialMultiplyMatrix(a, b);
-		parallelMultiplyMatrix(a, b);	
+		parallelMultiplyMatrix(a, b);
 	}
-	
+
 	/**
 	 * Returns the result of a sequential matrix multiplication
 	 * The two matrices are randomly generated
@@ -36,7 +36,7 @@ public class MatrixMultiplication {
 		}
 		return result_matrix;
 	}
-	
+
 	/**
 	 * Returns the result of a concurrent matrix multiplication
 	 * The two matrices are randomly generated
@@ -44,67 +44,67 @@ public class MatrixMultiplication {
 	 * @param b is the second matrix
 	 * @return the result of the multiplication
 	 * */
-        public static double[][] parallelMultiplyMatrix(double[][] a, double[][] b) {
-        	double[][] result_matrix = new double[MATRIX_SIZE][MATRIX_SIZE];
+	public static double[][] parallelMultiplyMatrix(double[][] a, double[][] b) {
+		double[][] result_matrix = new double[MATRIX_SIZE][MATRIX_SIZE];
 
-        	try {
-        		// Create a thread pool
-				ExecutorService executorService = Executors.newFixedThreadPool(NUMBER_THREADS);
+		try {
+			// Create a thread pool
+			ExecutorService executorService = Executors.newFixedThreadPool(NUMBER_THREADS);
 
-				for (int i = 0; i < MATRIX_SIZE; i++) {
-					for (int j = 0; j < MATRIX_SIZE; j++) {
-						executorService.execute(new ParallelMultiply(a.length, b.length, a, b, result_matrix));
-					}
-				}
-
-				// No other threads accept tasks
-				executorService.shutdown();
-
-				// Wait for threads to finish
-				executorService.awaitTermination(5, TimeUnit.SECONDS);
-				System.out.println("Multiplication succeessfully terminated: " + executorService.isTerminated());
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			return result_matrix;
-		}
-
-		static class ParallelMultiply implements Runnable {
-			private int row;
-			private  int col;
-			private double[][] a;
-			private double[][] b;
-			private double[][] result_matrix;
-
-			ParallelMultiply(int row, int col, double[][] a, double[][] b, double[][] result_matrix) {
-				this.row = row;
-				this.col = col;
-				this.a = a;
-				this.b = b;
-				this.result_matrix = result_matrix;
-			}
-
-			public void run() {
-				for (int k = 0; k < MATRIX_SIZE; k++) {
-					result_matrix[row][col] += a[row][k] * b[k][col];
+			for (int i = 0; i < MATRIX_SIZE; i++) {
+				for (int j = 0; j < MATRIX_SIZE; j++) {
+					executorService.execute(new ParallelMultiply(a.length, b.length, a, b, result_matrix));
 				}
 			}
+
+			// No other threads accept tasks
+			executorService.shutdown();
+
+			// Wait for threads to finish
+			executorService.awaitTermination(5, TimeUnit.SECONDS);
+			System.out.println("Multiplication succeessfully terminated: " + executorService.isTerminated());
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
-        
-        /**
-         * Populates a matrix of given size with randomly generated integers between 0-10.
-         * @param numRows number of rows
-         * @param numCols number of cols
-         * @return matrix
-         */
-        private static double[][] generateRandomMatrix (int numRows, int numCols) {
-             double matrix[][] = new double[numRows][numCols];
-        for (int row = 0 ; row < numRows ; row++ ) {
-            for (int col = 0 ; col < numCols ; col++ ) {
-                matrix[row][col] = (double) ((int) (Math.random() * 10.0));
-            }
-        }
-        return matrix;
-    }
-	
+		return result_matrix;
+	}
+
+	static class ParallelMultiply implements Runnable {
+		private int row;
+		private  int col;
+		private double[][] a;
+		private double[][] b;
+		private double[][] result_matrix;
+
+		ParallelMultiply(int row, int col, double[][] a, double[][] b, double[][] result_matrix) {
+			this.row = row;
+			this.col = col;
+			this.a = a;
+			this.b = b;
+			this.result_matrix = result_matrix;
+		}
+
+		public void run() {
+			for (int k = 0; k < MATRIX_SIZE; k++) {
+				result_matrix[row][col] += a[row][k] * b[k][col];
+			}
+		}
+	}
+
+	/**
+	 * Populates a matrix of given size with randomly generated integers between 0-10.
+	 * @param numRows number of rows
+	 * @param numCols number of cols
+	 * @return matrix
+	 */
+	private static double[][] generateRandomMatrix (int numRows, int numCols) {
+		double matrix[][] = new double[numRows][numCols];
+		for (int row = 0 ; row < numRows ; row++ ) {
+			for (int col = 0 ; col < numCols ; col++ ) {
+				matrix[row][col] = (double) ((int) (Math.random() * 10.0));
+			}
+		}
+		return matrix;
+	}
+
 }
