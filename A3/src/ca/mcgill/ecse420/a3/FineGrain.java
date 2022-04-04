0139c3fb3bcd6d35f310306c1e389e4a6faa7f5e
ParsaYadollahi <parsa.yadollahi@mail.mcgill.ca>
2022-04-04 14:09:04 -0400

    A3 Q2.1

--- A3/src/ca/mcgill/ecse420/a3/FineGrain.java
+++ A3/src/ca/mcgill/ecse420/a3/FineGrain.java
@@ -1,6 +1,107 @@
 package ca.mcgill.ecse420.a3;
 
-public class FineGrain<T> {
-  public static void main(String[] args) {}
+import ca.mcgill.ecse420.a3.Node;
 
+public class FineGrain<T> {
+  private Node head;
+
+  public FineGrain() {
+    head = new Node<>(Integer.MAX_VALUE);
+  }
+
+
+  public boolean add(T item) {
+    int key = item.hashCode();
+
+    head.lock();
+    Node prev = head;
+    try {
+      Node curr = prev.next;
+      curr.lock();
+      try {
+        while (curr.key < key) {
+          prev.unlock();
+          prev = curr;
+          curr = curr.next;
+          curr.lock();
+        }
+
+        if (curr.key == key) {
+          return false;
+        }
+
+        Node newNode = new Node(item);
+        newNode.next = curr;
+        prev.next = newNode;
+        return true;
+      } finally {
+        curr.unlock();
+      }
+    } finally {
+      prev.unlock();
+    }
+  }
+
+
+  public boolean remove(T item) {
+    Node prev = null;
+    Node curr = null;
+
+    int key = item.hashCode();
+    head.lock();
+
+    try {
+      prev = head;
+      curr = prev.next;
+      curr.lock();
+      try {
+        while (curr.key < key) {
+          prev.unlock();
+          prev = curr;
+          curr = curr.next;
+          curr.lock();
+        }
+        if (curr.key == key) {
+          prev.next = curr.next;
+          return true;
+        }
+        return false;
+      } finally {
+        curr.unlock();
+      }
+    } finally {
+      prev.unlock();
+    }
+  }
+
+  public boolean contains(T item) {
+    Node prev = null;
+    Node curr = null;
+
+    int key = item.hashCode();
+    head.lock();
+
+    try {
+      prev = head;
+      curr = prev.next;
+      curr.lock();
+      try {
+        while(curr.key < key) {
+          prev.unlock();
+          prev = curr;
+          curr = curr.next;
+          curr.lock();
+        }
+        if (curr.key == key){
+          return true;
+        } else {
+          return false;
+        }
+      } finally {
+        curr.unlock();
+      }
+    } finally {
+      prev.unlock();
+    }
+  }
 }
