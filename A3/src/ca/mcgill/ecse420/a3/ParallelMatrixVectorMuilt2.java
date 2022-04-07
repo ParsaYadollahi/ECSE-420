ff720f9c1dfcd4014a72ce57007bf69177236a3f
ParsaYadollahi <parsa.yadollahi@mail.mcgill.ca>
2022-04-07 17:46:28 -0400

    Q4.2 fixed

--- A3/src/ca/mcgill/ecse420/a3/ParallelMatrixVectorMuilt2.java
+++ A3/src/ca/mcgill/ecse420/a3/ParallelMatrixVectorMuilt2.java
@@ -2,69 +2,49 @@ package ca.mcgill.ecse420.a3;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 public class ParallelMatrixVectorMuilt2 {
+  public int NUM_THREADS;
+  public Matrix matrix;
+  public Vector vector;
 
-	public static double[][] matrix;
-	public static double[] vector;
-	public static int NUMBER_THREADS;
-	public static int MATRIX_SIZE;
+  public ParallelMatrixVectorMuilt2(Matrix matrix, Vector vector, int NUM_THREADS) {
+    this.matrix = matrix;
+    this.vector = vector;
+    this.NUM_THREADS = NUM_THREADS;
+  }
 
-	public ParallelMatrixVectorMuilt2(double[][] matrix, double[] vector,
-			int MATRIX_SIZE, int NUMBER_THREADS){
-		this.matrix = matrix;
-		this.vector = vector;
-		this.MATRIX_SIZE = MATRIX_SIZE;
-		this.NUMBER_THREADS = NUMBER_THREADS;
-	}
+  public static Vector mulitply(Matrix matrix, Vector vector, int NUM_THREADS) {
+    Vector c = new Vector(vector.dimension);
+    int MATRIX_SIZE = matrix.dimension;
+    ExecutorService exec = Executors.newFixedThreadPool(NUM_THREADS);
 
-	/**
-	 * @return the result of the multiplication
-	 * */
-	public static double[] parallelMultiplication() {
-    	double[] c = new double[MATRIX_SIZE];
-    	ExecutorService executor = Executors.newFixedThreadPool(NUMBER_THREADS);
-    	for (int i = 0; i < MATRIX_SIZE; i++) {
-    		executor.execute(new OneEntryMultiplication(matrix, vector, c, i, MATRIX_SIZE));
-    	}
-    	executor.shutdown();
-    	try {
-			executor.awaitTermination(30, TimeUnit.MINUTES);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	/*
-    	// if you want to print the result
-    	for (int i = 0; i < MATRIX_SIZE; i++) {
-			System.out.print(c[i] + " ");
-		}
-    	System.out.println("");
-		*/
+    for (int col = 0; col < MATRIX_SIZE; col++) {
+      exec.execute(new OneEntryMultiply(matrix, vector, c, col));
+    }
+    exec.shutdown();
 
-      for (int row = 0 ; row < c.length ; row++ ) {
-        System.out.print("| " + c[row] + " |\n");
+    return c;
+  }
+
+  static class OneEntryMultiply implements Runnable {
+    private Matrix a;
+    private Vector b, c;
+    private int MATRIX_SIZE, col;
+
+    public OneEntryMultiply(Matrix a, Vector b, Vector c, int col){
+      this.a = a;
+      this.b = b;
+      this.c = c;
+      this.MATRIX_SIZE = a.dimension;
+      this.col = col;
+    }
+
+    @Override
+    public void run() {
+      for (int row = 0; row < MATRIX_SIZE; row++) {
+        c.set(col, c.get(col) + a.get(col, row) * b.get(row));
       }
-    	return c;
-	}
-}
-
-//task class: one row of the matrix multiplied by the vector
-//to produce one entry of the result vector.
-class OneEntryMultiplication implements Runnable {
-	private double[][] a;
-	private double[] b, c;
-	private int i, MATRIX_SIZE;
-	public OneEntryMultiplication(double[][] m1, double[] m2, double[] m3,
-			int index, int m_size) {
-		a = m1; b = m2; c = m3;
-		i = index; MATRIX_SIZE = m_size;
-	}
-	@Override
-	public void run() {
-		for (int j = 0; j < MATRIX_SIZE; j++) {
-			c[i] = c[i] + a[i][j] * b[j];
-		}
-	}
+    }
+  }
 }
--- A3/src/ca/mcgill/ecse420/a3/TestMatrixVectorMulitply.java
+++ A3/src/ca/mcgill/ecse420/a3/TestMatrixVectorMulitply.java
@@ -1,58 +1,23 @@
 package ca.mcgill.ecse420.a3;
 
+import java.util.concurrent.ExecutionException;
+
 public class TestMatrixVectorMulitply {
+  private static final int NUM_THREADS = 3;
+  private static final int MATRIX_SIZE = 2000;
 
-	private static final int NUMBER_THREADS = 3;
-	private static final int MATRIX_SIZE = 2;
+  public static void main(String [] args) throws InterruptedException, ExecutionException {
+    Matrix m = new Matrix(MATRIX_SIZE);
+    m.generateRandomMatrix();
+    m.printMatrix();
 
-	public static void main(String[] args) {
-		double[][] matrix = generateRandomMatrix(MATRIX_SIZE, MATRIX_SIZE);
-		double[] vector = generateRandomVector(MATRIX_SIZE);
+    Vector v = new Vector(MATRIX_SIZE);
+    v.generateRandomVector();
+    v.printVector();
 
-    for (int row = 0 ; row < MATRIX_SIZE  ; row++ ) {
-			for (int col = 0 ; col < MATRIX_SIZE ; col++ ) {
-				System.out.print(matrix[row][col] + " ");
-			}
-      System.out.println();
-		}
+    System.out.println();
 
-    for (int row = 0 ; row < vector.length ; row++ ) {
-      System.out.print("| " + vector[row] + " |\n");
-    }
-
-		ParallelMatrixVectorMuilt2 par = new ParallelMatrixVectorMuilt2(matrix, vector, MATRIX_SIZE, NUMBER_THREADS);
-		double start = System.currentTimeMillis();
-		par.parallelMultiplication();
-		double end = System.currentTimeMillis();
-		System.out.println("Parallel: " + (end-start)/1000 + "s");
-	}
-
-	/**
-     * Populates a matrix of given size with randomly generated integers between 0-10.
-     * @param numRows number of rows
-     * @param numCols number of cols
-     * @return matrix
-     */
-    private static double[][] generateRandomMatrix (int numRows, int numCols) {
-        double matrix[][] = new double[numRows][numCols];
-        for (int row = 0 ; row < numRows ; row++ ) {
-            for (int col = 0 ; col < numCols ; col++ ) {
-                matrix[row][col] = (double) ((int) (Math.random() * 10.0));
-            }
-        }
-        return matrix;
-    }
-
-    /**
-     * Populates a vector of given size with randomly generated integers between 0-10.
-     * @param numCols number of cols
-     * @return vector
-     */
-    private static double[] generateRandomVector (int numCols) {
-        double vector[] = new double[numCols];
-        for (int col = 0 ; col < numCols ; col++ ) {
-            vector[col] = (double) ((int) (Math.random() * 10.0));
-        }
-        return vector;
-    }
+    Vector res = ParallelMatrixVectorMuilt2.mulitply(m, v, NUM_THREADS);
+    res.printVector();
+  }
 }
